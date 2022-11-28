af9e961f770d4812404a06ed5a782276269bfb95
sebastianocalella <sebastianocalella97@gmail.com>
2022-11-28 16:43:34 +0100

    code cleaning

--- snack 2/script.js
+++ snack 2/script.js
@@ -1,13 +1,11 @@
 const numberList = [];
 
 i=0;
-a=0;
 
 while (i<50){
     let number = parseInt(prompt('inserisci un numero:'));
     numberList.push(number);
     i+=number;
-    a++;
 }
 
 console.log(numberList);
