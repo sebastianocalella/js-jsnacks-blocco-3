06546d8b8d7ab8a81aaa2998a43eb46ca3b47f04
sebastianocalella <sebastianocalella97@gmail.com>
2022-11-28 16:43:03 +0100

    insert in array

--- snack 2/script.js
+++ snack 2/script.js
@@ -1,10 +1,15 @@
 const numberList = [];
 
 i=0;
+a=0;
 
 while (i<50){
     let number = parseInt(prompt('inserisci un numero:'));
+    numberList.push(number);
     i+=number;
+    a++;
 }
+
+console.log(numberList);
 
 console.log(i);
