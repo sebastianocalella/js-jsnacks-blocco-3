46723300ca28c69810ccf9b80c3459d2ef36bc33
sebastianocalella <sebastianocalella97@gmail.com>
2022-11-30 11:59:57 +0100

    stop working on it

--- snack 7/script.js
+++ snack 7/script.js
@@ -1,14 +1,25 @@
 const array = [1,33,55,64,33,22,10,44];
 
 
-function oddOrEven (arrayElement,length,divEven,divOdd){
-    for (i=0; i<length; i++){
-        if(arrayElement[i]%2===0){
-            document.getElementById(divEven).appendChild(arrayElement[i]);
-        } else {
-            document.getElementById(divOdd).appendChild(arrayElement[i]);
-        }
-    }
+function getNewPElement (content){
+    const newP = document.createElement('p');
+    newP.innerText = content;
+    return newP;
 }
 
-oddOrEven(array,array.length,'green','red');
+const divPari = document.getElementById('green');
+const divDispari = document.getElementById('red');
+
+
+
+// function oddOrEven (arrayElement,divEven,divOdd){
+//     for (i=0; i<arrayElement.length; i++){
+//         if(arrayElement[i]%2===0){
+//             document.getElementById(divEven).appendChild(arrayElement[i]);
+//         } else {
+//             document.getElementById(divOdd).appendChild(arrayElement[i]);
+//         }
+//     }
+// }
+
+// oddOrEven(array,'green','red');
