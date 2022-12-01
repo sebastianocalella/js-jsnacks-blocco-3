48063b111e77a212cb2b772b94e9df3371d206dd
sebastianocalella <sebastianocalella97@gmail.com>
2022-12-01 11:54:28 +0100

    snack

--- 2022-12-01/snack 1/script.js
+++ 2022-12-01/snack 1/script.js
@@ -1,16 +1,20 @@
 const startingList = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 const randomList =[];
 
-function randomIndex(List){
-    List.splice(Math.floor(Math.random() * (List.length + 1)),10);
-    return List;
+
+while (randomList.length<10){
+    const randomIndex = getRandomNumber(0, startingList.length - 1);
+    if(!randomList.includes(startingList[randomIndex])){
+        randomList.push(startingList[randomIndex]);
+    }
 }
 
-console.log(randomIndex(startingList));
+console.log(randomList);
 
-for (let i=0; i<10; i++){
-    
+function getRandomNumber(numMin, numMax){
+    if(numMin===numMax){
+        return numMax
+    }
+    return Math.floor(Math.random()*(numMax - numMin + 1) + numMin);
 }
-
-
 // Math.floor(Math.random() * (startingList.length + 1))
