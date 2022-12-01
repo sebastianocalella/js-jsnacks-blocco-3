1b83852aefb7a7a63630efe52ace0cf6b2160d08
sebastianocalella <sebastianocalella97@gmail.com>
2022-12-01 11:42:27 +0100

    function cleaning

--- 2022-12-01/snack 1/script.js
+++ 2022-12-01/snack 1/script.js
@@ -2,7 +2,7 @@ const startingList = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17
 const randomList =[];
 
 function randomIndex(List){
-    List.splice(Math.floor(Math.random() * (startingList.length + 1)),10);
+    List.splice(Math.floor(Math.random() * (List.length + 1)),10);
     return List;
 }
 
